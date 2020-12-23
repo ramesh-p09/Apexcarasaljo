f5766791dc26e4558d960e7c75567a66a870eb7c
Mallik <mallikarjunvetsa@outlook.com>
2020-12-23 07:36:19 +0530

    arm templates

---
+++ 01-storage.ps1
@@ -0,0 +1,8 @@
+$rg = 'arm-introduction-01'
+New-AzResourceGroup -Name $rg -Location northeurope -Force
+
+New-AzResourceGroupDeployment `
+    -Name 'new-storage' `
+    -ResourceGroupName $rg `
+    -TemplateFile '01-storage.json' `
+    -storagename 'test'
